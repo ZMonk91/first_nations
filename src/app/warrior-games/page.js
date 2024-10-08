644e4a26e1a6105874a2bbd14b899003cbe49b1d
Zachary Monk <zmonk@Zacharys-MacBook-Pro.local>
2024-10-08 11:48:18 -0700

    Minor bugfixes

--- src/app/warrior-games/page.js
+++ src/app/warrior-games/page.js
@@ -1,6 +1,4 @@
-"use client"; // This file is a client-side script
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt, faMapMarkerAlt, faMedal, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
